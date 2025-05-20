4c67fe0af8c825b22feb629d1e0bb56426177bef
din0sauro <auroramansilla1208@gmail.com>
2025-05-20 08:51:01 -0400

    Ajustes

--- public/js/bodega.js
+++ public/js/bodega.js
@@ -149,3 +149,22 @@ document.addEventListener("DOMContentLoaded", () => {
 function cerrarSesion() {
     fetch('/logout').then(() => window.location.href = '/login.html');
   }
+  
+/*let idUsuarioGlobal = null;
+
+ function informacionUsuario() {
+    fetch('/api/perfil')
+    .then(response => response.json())
+    .then(data => {
+        if (data.nombre && data.id) {
+            document.getElementById('nombreusuario').textContent = "Hola " + data.nombre;
+            idUsuarioGlobal = data.id;
+            console.log('ID del usuario guardado:', idUsuarioGlobal);
+        } else {
+            console.error('No se pudo obtener la información del usuario');
+        }
+    })
+    .catch(error => {
+        console.error('Error al obtener los datos del perfil:', error);
+    });
+}*/
